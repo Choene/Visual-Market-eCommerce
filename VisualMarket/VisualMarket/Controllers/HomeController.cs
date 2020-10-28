48f61fb78faa5fa348763e4a1166cb0ff10a664a
Choene <acchokoe@gmail.com>
2020-10-28 10:31:02 +0200

    Cached Commit

--- VisualMarket/VisualMarket/Controllers/HomeController.cs
+++ VisualMarket/VisualMarket/Controllers/HomeController.cs
@@ -1,9 +1,14 @@
 using Microsoft.AspNetCore.Mvc;
+using VisualMarket.VisualMarket.Models;
 
 namespace VisualMarket.Controllers
 {
     public class HomeController: Controller
     {
-        public IActionResult Index() => View();
+        private IVisualMarketRepository repositiry;
+        public HomeController(IVisualMarketRepository repo){
+            repositiry = repo;
+        }
+        public IActionResult Index() => View(repositiry.Products); //Controller to display list of products
     }
 }
